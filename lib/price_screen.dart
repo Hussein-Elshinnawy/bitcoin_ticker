249383caeed6ad93a25753a555fe13d3fffb6e34
Hussein-Elshinnawy <clorkthefirst@gmail.com>
2023-08-31 19:01:37 +0300

    silver level

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,8 +6,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'network_calls.dart';
 
-String apiKey='0FF4B2AA-9A97-4EFD-9CEC-A0B3FFAD45A5';
-String bitCoinUrl='https://rest.coinapi.io/v1/exchangerate/BTC/';
+String apiKey = '0FF4B2AA-9A97-4EFD-9CEC-A0B3FFAD45A5';
+String bitCoinUrl = 'https://rest.coinapi.io/v1/exchangerate/BTC/';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -15,7 +15,7 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  double bitUSD=0;
+  double bitCurrency = 0;
   String? SelectedCurrency = 'USD';
 
   DropdownButton<String> androidDropDown() {
@@ -34,10 +34,12 @@ class _PriceScreenState extends State<PriceScreen> {
         setState(() {
           SelectedCurrency = value;
         });
+        getBitCoin();
       },
     );
   }
-  CupertinoPicker iOSPicker(){
+
+  CupertinoPicker iOSPicker() {
     List<Text> dropdownMenuItems = [];
     for (String currency in currenciesList) {
       var item = Text(currency); //can be skipped
@@ -52,34 +54,32 @@ class _PriceScreenState extends State<PriceScreen> {
       children: dropdownMenuItems,
     );
   }
-  void getBitCoinUSD() async {
-    Uri url = Uri.parse('${bitCoinUrl}USD?apiKey=$apiKey');
-    NetwrokCall netwrokCall= NetwrokCall(url);
+
+  void getBitCoin() async {
+    Uri url = Uri.parse('${bitCoinUrl}$SelectedCurrency?apiKey=$apiKey');
+    NetwrokCall netwrokCall = NetwrokCall(url);
     try {
       var response = await netwrokCall.callApi();
       setState(() {
-        bitUSD = response['rate'];
+        bitCurrency = response['rate'];
       });
-    }
-    catch(e){
+    } catch (e) {
       print(e);
       setState(() {
-        bitUSD = 429;
+        bitCurrency = 429;
       });
     }
-
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getBitCoinUSD();
+    getBitCoin();
   }
 
   @override
   Widget build(BuildContext context) {
-
     // getBitCoinUSD();
     return Scaffold(
       appBar: AppBar(
@@ -101,7 +101,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = $bitUSD',
+                  '1 BTC = $bitCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -116,8 +116,8 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: Platform.isIOS? iOSPicker(): androidDropDown() ,
-            ),
+            child: Platform.isIOS ? iOSPicker() : androidDropDown(),
+          ),
         ],
       ),
     );
